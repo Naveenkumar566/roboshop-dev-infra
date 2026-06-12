cdefe8b74829e254a5b83ccc19c09234e8637029
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-12 21:23:42 +0530

    update code

---
+++ 30-bastion/bastion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# We are creating total of 50gb root disk, but 20gb is only for partioned
+#left over 30 gb we need to extend using below commands
+
+growpart /dev/nvme0n1p4 4                                          # We are increasing this folder size.  nvme0n1p4 is in /devices file(/dev), 4 is how many partions we need.
+lvextend -r -l+30g /dev/mapper/RootVG-homevol                      #lvextend will resize the volume, increasing to 30 gb, dev/mapper/RootVG-homevol is file name.
+xfs_growfs /home                                                   # mounting it on /home.
+
+
+yum install -y yum-utils shadow-utils                               #install terraform
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum install terraform
