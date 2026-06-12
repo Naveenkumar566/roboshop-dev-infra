3d8087f79cbcbf23e27760928c218dded160fbef
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-12 13:13:12 +0530

    actions

---
+++ 40-databases/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "mongodb" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = local.database_subnet_ids
+  vpc_security_group_ids = [local.mongodb_security_group_id]
+
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-mongodb"
+    },
+    local.common_tags
+  )
+}
+
+
+resource "terraform_data" "bootstrap" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.mongodb.private_ip
+  }
+
+  provisioner "file" {
+  source = "bootstrap.sh"                                                           # Local file path
+  destination = "/tmp/bootstrap.sh"                                                  #destination path on the remotemachine
+}
+
+
+  provisioner "local-exec" {
+    command = "bootstrap-hosts.sh"
+  }
+}
+
+     
