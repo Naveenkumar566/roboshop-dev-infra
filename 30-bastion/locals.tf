44d9903a5135770d051552a38474d6c5803c4cd9
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-13 11:16:28 +0530

    actions

--- 30-bastion/locals.tf
+++ 30-bastion/locals.tf
@@ -8,4 +8,3 @@ locals {
   public_subnet_ids         = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0] # public subnets are stored in stringList format to access required subnet we rae splitting them and accessing with 0'th element.                                                                                              
   bastion_security_group_id = data.aws_ssm_parameter.bastion_security_group_id.value
 }
- 
