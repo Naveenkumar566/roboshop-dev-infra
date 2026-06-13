b21119e39d4b905d0259738d042e283692a4f5fc
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-13 18:21:15 +0530

    actions

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+component=$1
 dnf install ansible -y
+
+
+cd /home/ec2-user
+git clone  https://github.com/Naveenkumar566/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf.git
+ansible-playbook -e component=$component roboshop.yaml
