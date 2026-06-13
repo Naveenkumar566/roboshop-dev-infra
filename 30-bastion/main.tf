baa65ac430b83e6e8ef594202c254737b3f830c3
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-13 10:26:05 +0530

    actions

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -5,16 +5,16 @@ resource "aws_instance" "bastion" {
   subnet_id              = local.public_subnet_ids
   vpc_security_group_ids = [local.bastion_security_group_id]
   iam_instance_profile   = aws_iam_instance_profile.bastion.name
-  user_data = file(bastion.sh)                                                               #once instance is provisioned, AWS executes the commands inside the bastion.sh.
+  user_data              = file("bastion.sh") #once instance is provisioned, AWS executes the commands inside the bastion.sh. 
 
-    root_block_device {
-      volume_size = 50
-      volume_type = "gp3"
-      tags = merge(
-     {
-      Name = "${var.project}-${var.environment}-bastion"                              #this tags are for EBS volume
-     },
-     local.common_tags
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    tags = merge(
+      {
+        Name = "${var.project}-${var.environment}-bastion" #this tags are for EBS volume
+      },
+      local.common_tags
     )
   }
 
@@ -39,7 +39,7 @@ resource "aws_iam_role" "bastion_role" {
         Effect = "Allow"
         Sid    = ""
         Principal = {
-          Service = "ec2.amazonaws.com"                                              #this points that this role is for EC2 instance
+          Service = "ec2.amazonaws.com" #this points that this role is for EC2 instance
         }
       },
     ]
@@ -55,12 +55,12 @@ resource "aws_iam_role" "bastion_role" {
 
 resource "aws_iam_role_policy_attachment" "bastion" {
   role       = aws_iam_role.bastion_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"                               #fetch it from AWS console "Administaractor Acess"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" #fetch it from AWS console "Administaractor Acess"
 }
 
 resource "aws_iam_instance_profile" "bastion" {
   name = "${var.project}-${var.environment}-bastion"
   role = aws_iam_role.bastion_role.name
 }
-  #check
+#check
 
